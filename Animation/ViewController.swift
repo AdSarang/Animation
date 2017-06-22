96b4b28a15e0be6827f67a08ab9b41159d9745ce
Advait <advait@advaits-macbook-pro.local>
2017-06-22 10:49:23 +0530

    version 1.0

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -9,7 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myTimer = Timer()
+    
+    var counter = 1
+    
+    @IBOutlet weak var scoobyImage: UIImageView!
+    
+    func startFunction(){
+        scoobyImage.image = UIImage(named: "frame\(counter).gif")
+        
+        counter = counter + 1
+        
+        if counter == 11{
+            counter = 1
+        }
+    }
+    
 
+    @IBAction func startButtonPressed(_ sender: Any) {
+        
+        myTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.startFunction), userInfo: nil, repeats: true)
+        
+    }
+    @IBAction func stopButtonPressed(_ sender: Any) {
+        
+        myTimer.invalidate()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
